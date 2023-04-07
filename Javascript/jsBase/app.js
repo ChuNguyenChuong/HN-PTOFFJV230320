551801dab00cc974f15b3e93a3bf642178cd17ea
Nguyen Chuong <chunguyenchuong2014bg@gmail.com>
2023-04-07 20:49:44 +0700

    js parrt 1

---
+++ Javascript/jsBase/app.js
@@ -0,0 +1,15 @@
+
+let stringValue = "" // ' '  ` `
+let booleanValue = true; // false
+let undefinedValue = undefined;
+let nullValue = null;
+
+
+//  toán tử số học : + - * / %   ++ --  =
+let counter = 10;
+const nextCounter = 15;
+
+// toán tử so sánh == === != !== > < >= <=
+
+const numberValue = 1000;
+const value = 200;
