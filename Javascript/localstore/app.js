c41cbb933bbf907d557149483393e088048a69fd
Nguyen Chuong <chunguyenchuong2014bg@gmail.com>
2023-05-05 20:18:41 +0700

    crud product

--- Javascript/localstore/app.js
+++ Javascript/localstore/app.js
@@ -1,116 +1,137 @@
-// bai 1
-
-// const valueInLocalStorage = localStorage.getItem("userName")
-
-// const userNameInput = document.getElementById("userNameInput")
-// const addBtn = document.getElementById("add")
-// const removeBtn = document.getElementById("remove")
-
-// const userNameShow = document.getElementById("userNameShow");
-
-// if (valueInLocalStorage) {
-//   addBtn.textContent = "edit"
-//   userNameInput.value = valueInLocalStorage
-//   userNameShow.innerText = valueInLocalStorage
-// }
-
-// addBtn.addEventListener("click", function (event) {
-//   const value = userNameInput.value;
-//   userNameShow.innerText = value;
-//   localStorage.setItem("userName", value)
-//   addBtn.textContent = "edit"
-// })
-
-// removeBtn.addEventListener("click", (e) => {
-//   localStorage.removeItem("userName")
-//   userNameInput.value = ""
-//   addBtn.textContent = "them"
-//   userNameShow.innerText = ""
-// })
-
-// userNameInput.addEventListener("change", (event) => {
-//   console.log(event.target.value);
-// })
-
-// bai 2
-
-// const userName = document.getElementById("userName")
-// const password = document.getElementById("password")
-
-// const userNameInLocalStorage = localStorage.getItem("userName")
-// const passInLocalStorage = localStorage.getItem("password")
-
-// userName.addEventListener("change", (e) => {
-//   localStorage.setItem("userName", e.target.value)
-//   sessionStorage.setItem("userName", e.target.value)
-// })
-
-// password.addEventListener("change", (e) => {
-//   localStorage.setItem("password", e.target.value)
-//   sessionStorage.setItem("password", e.target.value)
-// })
-
-// bai 3
-
-const array = [];
-
-function getDataLocal(key) {
-  const dataInLocal = localStorage.getItem(key);
-  const result = dataInLocal ? JSON.parse(dataInLocal) : []
-  return result
-}
-
-function setDataToLocal(key, value) {
-  const stringifyValue = JSON.stringify(value)
-  localStorage.setItem(key, stringifyValue)
-}
-
-const nameValue = document.getElementById("name")
-const priceValue = document.getElementById("price")
-const saveBtn = document.getElementById("save")
-const list = document.getElementById("list")
+const nameInput = document.getElementById("name")
+const priceInput = document.getElementById("price")
+const btnSave = document.getElementById("save")
+const ulTag = document.getElementById("ul")
 
 
-saveBtn.addEventListener("click", event => {
-  const products = getDataLocal()
-  console.log("🚀 ~ file: app.js:74 ~ products:", products)
-
-  // add to products list
-  let newProduct = {
-    name: nameValue.value,
-    price: priceValue.value
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
   }
-  products.push(newProduct);
-  console.log("🚀 ~ file: app.js:81 ~ products:", products)
-  // end
-  // set products to local
-  setDataToLocal("products", products)
+  return result;
+}
 
-  const ulHtml = document.getElementById("ul")
-  ulHtml.remove()
+function getProductsInLocal() {
+  const productsInLocal = localStorage.getItem("products");
+  const products = productsInLocal ? JSON.parse(productsInLocal) : []
+  return products
+}
 
-  const ulNew = document.createElement("ul")
-  ulNew.id = "ul"
-  console.log(products);
-  products.forEach((item, index) => {
-    const newLi = document.createElement("li")
-    const btnRemove = document.createElement("button")
-    btnRemove.textContent = "remove"
+function setProductsToLocal(productsList) {
+  localStorage.setItem("products", JSON.stringify(productsList))
+}
 
-    btnRemove.addEventListener("click", (e) => {
-      // index
-      newLi.remove()
-    })
+const deleteItem = (id) => {
+  const products = getProductsInLocal()
+  const newListProducts = products.filter(item => item.id !== id)
+  setProductsToLocal(newListProducts)
+  render()
+}
 
-    newLi.innerText = `${item.name} - ${item.price}`
+const editItem = (id) => {
+  const products = getProductsInLocal()
+  let indexUpdate;
 
-    newLi.appendChild(btnRemove)
-    ulNew.appendChild(newLi)
+  const product = products.find(function (item, index) {
+    if (item.id === id) {
+      indexUpdate = index
+    }
+    return item.id === id
   })
+  nameInput.value = product.name
+  priceInput.value = product.price
 
-  list.appendChild(ulNew)
+  btnSave.textContent = "update";
+
+  localStorage.setItem("idUpdate", JSON.stringify(id))
+  localStorage.setItem("indexUpdate", JSON.stringify(indexUpdate))
+
+}
 
 
+function render() {
+  const products = getProductsInLocal()
+
+  const ulTagOld = document.getElementById("ul")
+  ulTagOld.remove()
+
+
+  const ulTagNew = document.createElement("ul")
+  ulTagNew.id = "ul"
+
+
+
+  for (let i = 0; i < products.length; i++) {
+    const element = products[i];
+
+    const li = document.createElement("li")
+
+    const btnEdit = document.createElement("button")
+    btnEdit.textContent = "edit"
+    btnEdit.classList = "btn btn-edit"
+    btnEdit.addEventListener("click", () => editItem(element.id))
+
+    const btnDelete = document.createElement("button")
+    btnDelete.textContent = "delete"
+    btnDelete.classList = "btn btn-remove"
+    btnDelete.addEventListener("click", () => deleteItem(element.id))
+
+    const span = document.createElement("span")
+    span.textContent = element.name + " - " + element.price + " - " + element.id;
+
+    li.appendChild(btnEdit)
+    li.appendChild(btnDelete)
+    li.appendChild(span)
+
+
+    ulTagNew.appendChild(li)
+
+  }
+
+  const listItem = document.getElementById("list")
+
+  listItem.appendChild(ulTagNew)
+
+}
+
+render()
+
+btnSave.addEventListener("click", function (event) {
+  const idUpdate = localStorage.getItem("idUpdate")
+  const indexUpdate = localStorage.getItem("indexUpdate")
+  const products = getProductsInLocal()
+
+  if (idUpdate) {
+    const idEdit = JSON.parse(idUpdate)
+    const indexEdit = JSON.parse(indexUpdate)
+
+    const product = products.find(item => item.id === idEdit);
+
+    const name = nameInput.value
+    const price = priceInput.value
+    product.name = name
+    product.price = price
+    products.splice(indexEdit, 1, product)
+    btnSave.textContent = "save"
+    localStorage.removeItem("idUpdate")
+    localStorage.removeItem("indexUpdate")
+  } else {
+    const name = nameInput.value
+    const price = priceInput.value
+
+    const product = {
+      id: makeid(5),
+      name,
+      price
+    }
+    products.push(product);
+  }
+  console.log(products);
+  setProductsToLocal(products)
+  render()
 })
-
-
